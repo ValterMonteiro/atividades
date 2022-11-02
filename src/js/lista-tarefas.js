612f044217eaa63caff1c1bf146dfaf57b13c51b
ValterMonteiro <valtinms@gmail.com>
2022-11-02 19:41:28 -0400

    🚧✨ Funcionalidade:  Remover tarefa

--- src/js/lista-tarefas.js
+++ src/js/lista-tarefas.js
@@ -1,7 +1,8 @@
 import './nav.js'
 import '../css/lista-tarefas.css'
 
-const adicionarTarefa = (evento)=>{
+/* Adionar tarefas */
+const adicionarTarefa = (evento) => {
 
     evento.preventDefault();
 
@@ -27,9 +28,52 @@ const adicionarTarefa = (evento)=>{
 
     //atribuindo a tarefa a lista
     listaTarefa.appendChild(tarefa);
+
+    //excluindo tarefa
+    tarefa.appendChild(excluirTarefa());
 }
 
 const novaTarefa = document.querySelector('#btn-adicionar');
 
 //escutando o click para adicionar tarefa
 novaTarefa.addEventListener('click', adicionarTarefa)
+
+/* Excluir tarefa */
+
+const excluirTarefa = () => {
+
+    //criando elemetno
+    const botaoExcluir = document.createElement('btn-excluir');
+    
+    botaoExcluir.innerText = 'Excluir';
+
+    // adicionar um atributo ao elemento
+    botaoExcluir.setAttribute('className', 'Excluir');
+
+    //adicionando evento
+    botaoExcluir.addEventListener('click', (evento)=>{        
+
+            const botaoDeleta = evento.target;
+            
+            const tarefaExcluir = botaoDeleta.parentElement;
+        
+            tarefaExcluir.remove();
+        
+            return botaoDeleta;
+    });
+
+    return botaoExcluir;
+   
+}
+
+/* const deletarTarefa = (evento)=>{
+
+    const botaoDeleta = evento.target;
+    
+    const tarefaExcluir = botaoDeleta.parentElement;
+
+    tarefaExcluir.remove();
+
+    return botaoDeleta;    
+
+} */

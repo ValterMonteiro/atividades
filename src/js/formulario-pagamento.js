a985da61915903056a6d13fbfee343ca778bdcc8
ValterMonteiro <valtinms@gmail.com>
2022-11-27 13:22:31 -0400

    💄 update

--- src/js/formulario-pagamento.js
+++ src/js/formulario-pagamento.js
@@ -2,25 +2,23 @@ import './nav.js'
 import '../css/formulario-pagamento.css'
 
 
-const pegarInformacao =() =>{
-    
-    
-}
-
 /* numero do cartão */
 const inputNumero = document.querySelector('.input-numero');
 const inputNome = document.querySelector('.input-nome');
-const inputData = document.querySelector('.input-data');
+const inputAno = document.querySelector('.input-ano');
+const inputMes = document.querySelector('.input-mes');
 const inputCCV = document.querySelector('.input-CodSeguranca');
 
 const cardNumber = document.getElementById('card-number');
 const cardHolder = document.getElementById('card-holder');
-const cardData = document.getElementById('expiration');
+const cardMes = document.getElementById('expiration-mes');
+const cardAno = document.getElementById('expiration-ano');
 const cardCCV = document.getElementById('ccv-blok');
 
 inputNumero.addEventListener('input', pegarNumero);
 inputNome.addEventListener('input', pegarNome);
-inputData.addEventListener('input', pegarData);
+inputAno.addEventListener('input', pegarAno);
+inputMes.addEventListener('input', pegarMes);
 inputCCV.addEventListener('input', pegarCCV);
 
 function pegarNumero(e) {
@@ -28,13 +26,27 @@ function pegarNumero(e) {
 }
 
 function pegarNome(e) {
-    cardHolder.textContent = e.target.value;
-  }
+  cardHolder.textContent = e.target.value;
+}
 
-function pegarData(e) {
-  cardData.textContent = e.target.value;
+function pegarAno(e) {
+  cardAno.textContent = e.target.value;
+}
+
+function pegarMes(e){
+  cardMes.textContent = e.target.value;
 }
 
 function pegarCCV(e) {
   cardCCV.textContent = e.target.value;
+}
+
+document.querySelector('.input-CodSeguranca').onmouseenter = () => {
+  document.querySelector('.credit-card-front').style.transform = 'perspective(1000px) rotateY(-180deg)';
+  document.querySelector('.credit-card-back').style.transform = 'perspective(1000px) rotateY(0deg)';
+}
+
+document.querySelector('.input-CodSeguranca').onmouseleave = () => {
+  document.querySelector('.credit-card-front').style.transform = 'perspective(1000px) rotateY(0deg)';
+  document.querySelector('.credit-card-back').style.transform = 'perspective(1000px) rotateY(-180deg)';
 }
